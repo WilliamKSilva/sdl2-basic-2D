a44f07b76b042fd326886dd6ca42a4fcb781dcca
William Kelvin da Silva <williamkds@wklaptop>
2024-01-17 16:02:20 -0300

    buid: change build system to use cmake instead of Visual Studio build system

---
+++ opengl-basic-2D/build/CMakeFiles/renderer.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/renderer.dir/GL.cpp.o"
+  "CMakeFiles/renderer.dir/GL.cpp.o.d"
+  "CMakeFiles/renderer.dir/glad.c.o"
+  "CMakeFiles/renderer.dir/glad.c.o.d"
+  "CMakeFiles/renderer.dir/main.cpp.o"
+  "CMakeFiles/renderer.dir/main.cpp.o.d"
+  "CMakeFiles/renderer.dir/shader.cpp.o"
+  "CMakeFiles/renderer.dir/shader.cpp.o.d"
+  "renderer"
+  "renderer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/renderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
